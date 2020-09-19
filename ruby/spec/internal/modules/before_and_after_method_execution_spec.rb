dff8dada3cb62e3811c7a620c7ba855b1b29ff19
patricio.filice@gmail.com <king light1>
2020-09-19 20:47:52 -0300

    Corrección estructura de tests unitarios

--- ruby/spec/internal/modules/before_and_after_method_execution_spec.rb
+++ ruby/spec/internal/modules/before_and_after_method_execution_spec.rb
@@ -3,41 +3,33 @@ require_relative '../../spec_helper'
 describe BeforeAndAfterMethodExecution do
   let(:before_and_after_method_execution) { Object.new; Object.extend(BeforeAndAfterMethodExecution) }
 
-  describe 'when before_method_blocks is invoked' do
+  describe 'Behaviour' do
     it 'before_method_blocks should return an empty array' do
       before_method_blocks = before_and_after_method_execution.before_method_blocks
       expect(before_method_blocks).to eq []
       expect(before_method_blocks.any?).to be_falsey
     end
-  end
 
-  describe 'when after_method_blocks is invoked' do
     it 'after_method_blocks should return an empty array' do
       after_method_blocks = before_and_after_method_execution.after_method_blocks
       expect(after_method_blocks).to eq []
       expect(after_method_blocks.any?).to be_falsey
     end
-  end
 
-  describe 'when before_each_call is invoked' do
     it 'before_each_call should return an empty array' do
       before_method_block = proc {}
       before_and_after_method_execution.before_each_call &before_method_block
       before_method_blocks = before_and_after_method_execution.before_method_blocks
       expect(before_method_blocks).to eq [before_method_block]
     end
-  end
 
-  describe 'when after_each_call is invoked' do
     it 'after_each_call should return an empty array' do
       after_method_block = proc {}
       before_and_after_method_execution.after_each_call &after_method_block
       before_method_blocks = before_and_after_method_execution.after_method_blocks
       expect(before_method_blocks).to eq [after_method_block]
     end
-  end
 
-  describe 'when redefine_method is invoked' do
     it 'redefine_method should call every before and after block' do
       before_method_called = false
       method_called = false
@@ -61,9 +53,7 @@ describe BeforeAndAfterMethodExecution do
       expect(method_called).to be_truthy
       expect(after_method_called).to be_truthy
     end
-  end
 
-  describe 'when redefine_method is invoked' do
     it 'redefine_method should call every before and after block with arity one' do
       before_method_called = false
       method_called = false
