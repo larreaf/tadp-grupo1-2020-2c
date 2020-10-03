65b334586eaabd83569937707f19ca1027c41603
Brian Luna <brian.damian.luna@gmail.com>
2020-10-02 22:21:53 -0300

    Agrego tests en conditions envelope method

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -5,3 +5,4 @@ require_relative '../lib/internal/modules/before_and_after_method_execution_modu
 require_relative '../lib/internal/modules/proc_arity_restrainer_module'
 require_relative '../lib/internal/modules/consistent_object_module'
 require_relative '../lib/internal/modules/method_enveloper_module'
+require_relative '../lib/internal/classes/conditions_envelope_method'
