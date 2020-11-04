6d6a16305878a491fd1f18ab675e3ced4a5d88b5
Brian Luna <brian.damian.luna@gmail.com>
2020-11-03 21:46:55 -0300

    Reescritos los parsers string y anyChar.

--- scala/src/main/scala/parser_combinators/internal/cases/objects/anyChar.scala
+++ scala/src/main/scala/parser_combinators/internal/cases/objects/anyChar.scala
@@ -1,8 +1,17 @@
 package parser_combinators.internal.cases.objects
 
 import parser_combinators.internal.auxiliars.RegularExpressionParser
+import parser_combinators.internal.cases.classes.ParseResult
+import parser_combinators.internal.mixins.Parser
 
-case object anyChar extends RegularExpressionParser[Char]("^-?\\w".r) {}
+import scala.util.Try
+
+case object anyChar extends Parser[Char] {
+  override def apply(source: String): Try[ParseResult[Char]] = source match {
+    case string: String if !string.isEmpty => Try(ParseResult(string.charAt(0), string.substring(1)))
+    case _ => Try(throw new Error)
+  }
+}
 
 //case object anyChar extends StringParser[Char] {
 //  override protected def result(source: String): Try[Char] = source match {
