99b628b3c3ce28d184d5dd988406ea54d7e8871b
patricio.filice@gmail.com <patricio.filice@gmail.com>
2020-11-08 21:19:16 -0300

    Formateado string de ejemplo para ser parseable.

--- scala/src/main/scala/figure_parsers/main_figure_parsers.scala
+++ scala/src/main/scala/figure_parsers/main_figure_parsers.scala
@@ -27,11 +27,11 @@ object main_figure_parsers extends App {
 
   val scaleParsed = scaleParser(scale)
 
-  val secondGroup = "grupo(color[0, 0, 0](rectangulo[0 @ 0, 400 @ 400]), color[200, 70, 0](rectangulo[0 @ 0, 180 @ 150]), color[250, 250, 250](grupo(rectangulo[186 @ 0, 400 @ 150],rectangulo[186 @ 159, 400 @ 240], rectangulo[0 @ 159, 180 @ 240], rectangulo[45 @ 248, 180 @ 400], rectangulo[310 @ 248, 400 @ 400], rectangulo[186 @ 385, 305 @ 400])), color[30, 50, 130](rectangulo[186 @ 248, 305 @ 380]), color[250, 230, 0](rectangulo[0 @ 248, 40 @ 400]))"
+  val secondGroup = "grupo(color[0, 0, 0](rectangulo[0 @ 0, 400 @ 400]),color[200, 70, 0](rectangulo[0 @ 0, 180 @ 150]),color[250, 250, 250](grupo(rectangulo[186 @ 0, 400 @ 150],rectangulo[186 @ 159, 400 @ 240],rectangulo[0 @ 159, 180 @ 240],rectangulo[45 @ 248, 180 @ 400],rectangulo[310 @ 248, 400 @ 400],rectangulo[186 @ 385, 305 @ 400])),color[30, 50, 130](rectangulo[186 @ 248, 305 @ 380]),color[250, 230, 0](rectangulo[0 @ 248, 40 @ 400]))"
 
   val parsedSecondGroup = groupParser(secondGroup)
 
-  val figures = "escala[1.45, 1.45](grupo(color[0, 0, 0](rectangulo[0 @ 0, 400 @ 400]), color[200, 70, 0](rectangulo[0 @ 0, 180 @ 150]), color[250, 250, 250](grupo(rectangulo[186 @ 0, 400 @ 150], rectangulo[186 @ 159, 400 @ 240], rectangulo[0 @ 159, 180 @ 240], rectangulo[45 @ 248, 180 @ 400], rectangulo[310 @ 248, 400 @ 400], rectangulo[186 @ 385, 305 @ 400])), color[30, 50, 130](rectangulo[186 @ 248, 305 @ 380]), color[250, 230, 0](rectangulo[0 @ 248, 40 @ 400])))"
+  val figures = "escala[1.45, 1.45](grupo(color[0, 0, 0](rectangulo[0 @ 0, 400 @ 400]),color[200, 70, 0](rectangulo[0 @ 0, 180 @ 150]),color[250, 250, 250](grupo(rectangulo[186 @ 0, 400 @ 150],rectangulo[186 @ 159, 400 @ 240],rectangulo[0 @ 159, 180 @ 240],rectangulo[45 @ 248, 180 @ 400],rectangulo[310 @ 248, 400 @ 400],rectangulo[186 @ 385, 305 @ 400])),color[30, 50, 130](rectangulo[186 @ 248, 305 @ 380]),color[250, 230, 0](rectangulo[0 @ 248, 40 @ 400])))"
 
   val parsedFigures = figureParser(figures)
 
