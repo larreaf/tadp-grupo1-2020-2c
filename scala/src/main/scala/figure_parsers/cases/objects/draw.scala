10ab77f2b4d1a7cb86200309109e6e3c956a80e6
Federico Larrea <fedeml.95@gmail.com>
2020-11-11 12:38:01 -0300

    Mejoro el dibujado de imágenes

--- scala/src/main/scala/figure_parsers/cases/objects/draw.scala
+++ scala/src/main/scala/figure_parsers/cases/objects/draw.scala
@@ -5,31 +5,28 @@ import figure_parsers.internal.Figure.{Circle, Colour, Group, Rectangle, Relocat
 import scalafx.scene.paint.Color
 import tadp.internal.TADPDrawingAdapter
 
-case class draw(figure: Figure) extends (TADPDrawingAdapter => Any){
-  def apply(adapter: TADPDrawingAdapter): Any = figure match {
-    case Group(figures) => for (figure <- figures) yield draw(figure)(adapter)
+case class draw(figure: Figure) extends (TADPDrawingAdapter => TADPDrawingAdapter){
+  def apply(adapter: TADPDrawingAdapter): TADPDrawingAdapter = figure match {
+    case Group(figures) => figures.foldLeft(adapter) { (_adapter, fig) => draw(fig)(_adapter)}
     case Colour(red, green, blue, _figure) => {
-      val _adapter = adapter.beginColor(Color.rgb(red, green, blue))
-      draw(_figure)(_adapter)
-      _adapter.end()
+      draw(_figure)(adapter.beginColor(Color.rgb(red, green, blue)))
+        .end()
     }
     case Rotate(grade, _figure) => {
-      val _adapter = adapter.beginRotate(grade.toDouble)
-      draw(_figure)(_adapter)
-      _adapter.end()
+      draw(_figure)(adapter.beginRotate(grade.toDouble))
+        .end()
     }
     case Scale(x, y, _figure) => {
-      val _adapter = adapter.beginScale(x, y)
-      draw(_figure)(_adapter)
-      _adapter.end()
+      draw(_figure)(adapter.beginScale(x, y))
+        .end()
     }
     case Relocate(x, y, _figure) => {
-      val _adapter = adapter.beginTranslate(x.toDouble, y.toDouble)
-      draw(_figure)(_adapter)
-      _adapter.end()
+      draw(_figure)(adapter.beginTranslate(x.toDouble, y.toDouble))
+        .end()
     }
     case Rectangle(coordinate1, coordinate2) => adapter.rectangle((coordinate1.x.toDouble, coordinate1.y.toDouble), (coordinate2.x.toDouble, coordinate2.y.toDouble))
     case Triangle(coordinate1, coordinate2, coordinate3) => adapter.triangle((coordinate1.x.toDouble, coordinate1.y.toDouble), (coordinate2.x.toDouble, coordinate2.y.toDouble), (coordinate3.x.toDouble, coordinate3.y.toDouble))
     case Circle(center, radius) => adapter.circle((center.x.toDouble, center.y.toDouble), radius.toDouble)
   }
 }
+
