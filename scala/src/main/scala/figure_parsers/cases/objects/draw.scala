e6391e17c3310a79e065240c4a81e10b7d90fa42
Federico Larrea <fedeml.95@gmail.com>
2020-11-11 14:20:26 -0300

    Agrego consola interactiva para archivos

--- scala/src/main/scala/figure_parsers/cases/objects/draw.scala
+++ scala/src/main/scala/figure_parsers/cases/objects/draw.scala
@@ -8,22 +8,11 @@ import tadp.internal.TADPDrawingAdapter
 case class draw(figure: Figure) extends (TADPDrawingAdapter => TADPDrawingAdapter){
   def apply(adapter: TADPDrawingAdapter): TADPDrawingAdapter = figure match {
     case Group(figures) => figures.foldLeft(adapter) { (_adapter, fig) => draw(fig)(_adapter)}
-    case Colour(red, green, blue, _figure) => {
-      draw(_figure)(adapter.beginColor(Color.rgb(red, green, blue)))
-        .end()
-    }
-    case Rotate(grade, _figure) => {
-      draw(_figure)(adapter.beginRotate(grade.toDouble))
-        .end()
-    }
-    case Scale(x, y, _figure) => {
-      draw(_figure)(adapter.beginScale(x, y))
-        .end()
-    }
-    case Relocate(x, y, _figure) => {
-      draw(_figure)(adapter.beginTranslate(x.toDouble, y.toDouble))
-        .end()
-    }
+    case Colour(red, green, blue, _figure) => draw(_figure)(adapter.beginColor(Color.rgb(red, green, blue))).end()
+    case Rotate(grade, _figure) => draw(_figure)(adapter.beginRotate(grade.toDouble)).end()
+    case Scale(x, y, _figure) => draw(_figure)(adapter.beginScale(x, y)).end()
+    case Relocate(x, y, _figure) => draw(_figure)(adapter.beginTranslate(x.toDouble, y.toDouble)).end()
+
     case Rectangle(coordinate1, coordinate2) => adapter.rectangle((coordinate1.x.toDouble, coordinate1.y.toDouble), (coordinate2.x.toDouble, coordinate2.y.toDouble))
     case Triangle(coordinate1, coordinate2, coordinate3) => adapter.triangle((coordinate1.x.toDouble, coordinate1.y.toDouble), (coordinate2.x.toDouble, coordinate2.y.toDouble), (coordinate3.x.toDouble, coordinate3.y.toDouble))
     case Circle(center, radius) => adapter.circle((center.x.toDouble, center.y.toDouble), radius.toDouble)
--- scala/src/main/scala/tadp/TADPDrawingApp.scala
+++ scala/src/main/scala/tadp/TADPDrawingApp.scala
@@ -1,15 +1,31 @@
 package tadp
 
+import figure_parsers.cases.objects.{draw, figureParser}
+import figure_parsers.internal.Figure
+import figure_parsers.main_figure_parsers.testDraw
 import scalafx.scene.paint.Color
 import tadp.internal.TADPDrawingAdapter
 
+import scala.io.Source
+
 object TADPDrawingApp extends App {
-// Ejemplo de uso del drawing adapter:
-  TADPDrawingAdapter
-    .forScreen { adapter =>
-      adapter
-        .beginColor(Color.rgb(100, 100, 100))
-        .rectangle((200, 200), (400, 400))
-        .end()
+  
+  val parseAndDrawFile = (imageName: String, adapter: TADPDrawingAdapter) =>{
+    var figureFile = ""
+
+    val bufferedSource = Source.fromFile("resources/"+imageName+".txt")
+    for (line <- bufferedSource.getLines) {
+      figureFile = figureFile.concat(line)
     }
+    bufferedSource.close
+    val testDraw = figureParser(figureFile)
+    draw(testDraw.get.parsed)(adapter)
+  }
+
+
+  TADPDrawingAdapter.forInteractiveScreen { (imageDescription, adapter) =>
+    parseAndDrawFile(imageDescription, adapter)
+  }
+
 }
+
