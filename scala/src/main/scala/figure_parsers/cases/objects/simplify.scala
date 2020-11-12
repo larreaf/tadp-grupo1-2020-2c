f89791cef1cb880686f7c97d9fd81abac3e51df4
patricio.filice@gmail.com <patricio.filice@gmail.com>
2020-11-12 09:55:49 -0300

    Refactor y fix en simplificado de la expresión.

--- scala/src/main/scala/figure_parsers/cases/objects/simplify.scala
+++ scala/src/main/scala/figure_parsers/cases/objects/simplify.scala
@@ -1,7 +1,7 @@
 package figure_parsers.cases.objects
 
 import figure_parsers.internal.Figure
-import figure_parsers.internal.Figure.{Circle, Colour, Group, Rectangle, Relocate, Rotate, Scale, Triangle}
+import figure_parsers.internal.Figure._
 
 import scala.util.{Failure, Success, Try}
 
@@ -9,7 +9,7 @@ import scala.util.{Failure, Success, Try}
 case object simplify extends Function[Figure, Figure] {
 
   def apply(figureTree: Figure): Figure = figureTree match {
-    case Group(figures) => {
+    case Group(figures) =>
       val commonColor = Try(figures.head.asInstanceOf[Colour])
       val figuresInsideColors = groupWithCommonColor(commonColor)(figures)
 
@@ -17,7 +17,6 @@ case object simplify extends Function[Figure, Figure] {
         case Success(figs) => Colour(commonColor.get.red, commonColor.get.green, commonColor.get.blue, simplify(Group(figs)))
         case Failure(_) => Group(for (figure <- figures) yield simplify(figure))
       }
-    }
 
     case Colour(_, _, _, Colour(_red, _green, _blue, figure)) => Colour(_red, _green, _blue, simplify(figure))
     case Rotate(grade, Rotate(_grade, figure)) => Rotate(grade + _grade, simplify(figure))
@@ -27,11 +26,9 @@ case object simplify extends Function[Figure, Figure] {
     case Colour(red, green, blue, figure) => Colour(red, green, blue, simplify(figure))
     case Rotate(grade, figure) => if(grade == 0) simplify(figure) else Rotate(grade, simplify(figure))
     case Scale(x, y, figure) => if(x == 1 && y == 1) simplify(figure) else Scale(x, y, simplify(figure))
-    case Relocate(x, y, figure) => if(x == 0 && y == 0) simplify(figure) else Relocate(0, 0, simplify(figure))
+    case Relocate(x, y, figure) => if(x == 0 && y == 0) simplify(figure) else Relocate(x, y, simplify(figure))
 
-    case Rectangle(coordinate1, coordinate2) => Rectangle(coordinate1, coordinate2)
-    case Triangle(coordinate1, coordinate2, coordinate3) => Triangle(coordinate1, coordinate2, coordinate3)
-    case Circle(center, radius) => Circle(center, radius)
+    case figure: Figure => figure
   }
 }
 
