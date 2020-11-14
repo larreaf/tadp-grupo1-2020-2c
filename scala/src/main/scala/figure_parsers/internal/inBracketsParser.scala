8ff75ca1bb96c0c730e31bafe884209077bdb5dd
patricio.filice@gmail.com <patricio.filice@gmail.com>
2020-11-14 13:08:50 -0300

    Simplificados inBrackets e inParentheses parsers.

--- scala/src/main/scala/figure_parsers/internal/inBracketsParser.scala
+++ scala/src/main/scala/figure_parsers/internal/inBracketsParser.scala
@@ -1,13 +1,7 @@
 package figure_parsers.internal
 
-import parser_combinators.internal.cases.classes.ParseResult
 import parser_combinators.internal.mixins.Parser
 
-import scala.util.Try
-
-case class inBracketsParser[Parsed](parser: Parser[Parsed]) extends Parser[Parsed] {
-  val innerParser: Parser[Parsed] = inParser('[', parser, ']')
-
-  override def apply(source: String): Try[ParseResult[Parsed]] = this.innerParser(source)
+case object inBracketsParser {
+  def apply[Parsed](parser: Parser[Parsed]): inParser[Parsed] = inParser('[', parser, ']')
 }
-
