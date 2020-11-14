5e8d37b2aab4b63942fbe61c2fe55a6fdcac733c
patricio.filice@gmail.com <patricio.filice@gmail.com>
2020-11-14 12:28:14 -0300

    Simplificados parsers básicos.

--- scala/src/main/scala/parser_combinators/internal/cases/classes/positiveClosure.scala
+++ scala/src/main/scala/parser_combinators/internal/cases/classes/positiveClosure.scala
@@ -9,10 +9,6 @@ case class positiveClosure[Parsed](parser: Parser[Parsed]) extends Parser[List[P
   val innerParser: Parser[(Parsed, List[Parsed])] = parser <> kleeneClosure(parser)
 
   override def apply(source: String): Try[ParseResult[List[Parsed]]] = {
-    this.innerParser(source)
-        .map(combinedParseResult => {
-          val parsedTuple = combinedParseResult.parsed
-          ParseResult(parsedTuple._1 +: parsedTuple._2, combinedParseResult.remnant)
-        })
+    this.innerParser.map(tupleParsed => tupleParsed._1 +: tupleParsed._2)(source)
   }
 }
--- scala/src/main/scala/parser_combinators/internal/cases/objects/integer.scala
+++ scala/src/main/scala/parser_combinators/internal/cases/objects/integer.scala
@@ -10,12 +10,11 @@ case object integer extends Parser[Integer] {
   val parser: Parser[(Option[Char], Integer)] = char('-').opt <> naturalWithZero
 
   override def apply(source: String): Try[ParseResult[Integer]] = {
-    this.parser(source)
-        .map(parseResult => {
-          val parsedTuple = parseResult.parsed
-          val optionalMinusSign = parsedTuple._1.map(minusSing => minusSing.toString).getOrElse("")
-          val digits = parsedTuple._2
-          ParseResult(s"$optionalMinusSign$digits".toInt, parseResult.remnant)
-        })
+    this.parser
+      .map[Integer](tupleParsed => {
+        val optionalMinusSign = tupleParsed._1.map(minusSing => minusSing.toString).getOrElse("")
+        val digits = tupleParsed._2
+        s"$optionalMinusSign$digits".toInt
+      })(source)
   }
 }
