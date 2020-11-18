1082feadf1c264278c53d227cc3318b5547118f5
patricio.filice@gmail.com <patricio.filice@gmail.com>
2020-11-18 01:49:15 -0300

    Delegada responsabilidad de simplify a Drawable

--- scala/src/main/scala/figure_parsers/internal/Drawable.scala
+++ scala/src/main/scala/figure_parsers/internal/Drawable.scala
@@ -2,36 +2,69 @@ package figure_parsers.internal
 
 import figure_parsers.cases.Coordinates2D
 
-sealed trait Drawable
-sealed trait Figure extends Drawable
+sealed trait Drawable {
+  def simplify: Drawable
+  def drawable: Drawable
+}
+sealed trait Figure extends Drawable {
+  override def drawable: Drawable = this
+}
+sealed trait AtomicFigure extends Figure {
+  override def simplify: Drawable = this
+}
 sealed trait Transformation extends Drawable {
-  def drawable(): Drawable = ???
-
-  def copyWithDrawable(drawable: Drawable): Transformation = ???
+  val drawable: Drawable
+  def change(drawable: Drawable): Transformation
 }
 object Figure {
-  case class Triangle(coordinate1: Coordinates2D, coordinate2: Coordinates2D, coordinate3: Coordinates2D) extends Figure
-  case class Rectangle(coordinate1: Coordinates2D, coordinate2: Coordinates2D) extends Figure
-  case class Circle(center: Coordinates2D, radius: Double) extends Figure
-  case class Group(figures: List[Drawable]) extends Figure
-  case class Colour(red: Int, green: Int, blue: Int, override val drawable: Drawable) extends Transformation {
-    def equals(colour: Colour): Boolean = this.red == colour.red && this.green == colour.green && this.blue == colour.blue
+  case class Triangle(coordinate1: Coordinates2D, coordinate2: Coordinates2D, coordinate3: Coordinates2D) extends AtomicFigure
+  case class Rectangle(coordinate1: Coordinates2D, coordinate2: Coordinates2D) extends AtomicFigure
+  case class Circle(center: Coordinates2D, radius: Double) extends AtomicFigure
+  case class Group(drawables: List[Drawable]) extends Figure {
+    override def simplify: Drawable = {
+      drawables.head match {
+        case transformation: Transformation if drawables.slice(1, drawables.length)
+                                                        .forall(d => d == transformation) =>
+          transformation.change(this.simplifiedGroup(drawables.map(d => d.drawable)))
+        case _ => this.simplifiedGroup(this.drawables)
+      }
+    }
 
-    override def copyWithDrawable(_drawable: Drawable): Transformation = this.copy(drawable = _drawable)
+    private def simplifiedGroup(drawables: List[Drawable]): Drawable = this.copy(drawables = drawables.map(d => d.simplify))
   }
-  case class Scale(x: Double, y: Double, override val drawable: Drawable) extends Transformation {
-    def equals(scale: Scale): Boolean = this.x == scale.x && this.y == scale.y
-
-    override def copyWithDrawable(_drawable: Drawable): Transformation = this.copy(drawable = _drawable)
+  case class Colour(red: Int, green: Int, blue: Int, drawable: Drawable) extends Transformation {
+    override def hashCode(): Int = (red, green, blue).##
+    override def change(drawable: Drawable): Transformation = this.copy(drawable = drawable)
+    override def simplify: Drawable = this match {
+      case Colour(_, _, _, Colour(_red, _green, _blue, drawable)) => Colour(_red, _green, _blue, drawable.simplify)
+      case Colour(red, green, blue, drawable) => Colour(red, green, blue, drawable.simplify)
+    }
   }
-  case class Rotate(grade: Double, override val drawable: Drawable) extends Transformation {
-    def equals(rotate: Rotate): Boolean = this.grade == rotate.grade
-
-    override def copyWithDrawable(_drawable: Drawable): Transformation = this.copy(drawable = _drawable)
+  case class Scale(x: Double, y: Double, drawable: Drawable) extends Transformation {
+    override def hashCode(): Int = (x, y).##
+    override def change(drawable: Drawable): Transformation = this.copy(drawable = drawable)
+    override def simplify: Drawable = this match {
+      case Scale(x, y, Scale(_x, _y, drawable)) => Scale(x * _x, y * _y, drawable.simplify)
+      case Scale(1, 1, drawable) => drawable.simplify
+      case Scale(_, _, drawable) => this.change(drawable.simplify)
+    }
   }
-  case class Relocate(x: Double, y: Double, override val drawable: Drawable) extends Transformation {
-    def equals(relocate: Relocate): Boolean = this.x == relocate.x && this.y == relocate.y
-
-    override def copyWithDrawable(_drawable: Drawable): Transformation = this.copy(drawable = _drawable)
+  case class Rotate(grade: Double, drawable: Drawable) extends Transformation {
+    override def hashCode(): Int = grade.##
+    override def change(drawable: Drawable): Transformation = this.copy(drawable = drawable)
+    override def simplify: Drawable = this match {
+      case Rotate(grade, Rotate(_grade, drawable)) => Rotate(grade + _grade, drawable.simplify)
+      case Rotate(0, drawable) => drawable.simplify
+      case Rotate(grade, drawable) => Rotate(grade, drawable.simplify)
+    }
+  }
+  case class Relocate(x: Double, y: Double, drawable: Drawable) extends Transformation {
+    override def hashCode(): Int = (x, y).##
+    override def change(drawable: Drawable): Transformation = this.copy(drawable = drawable)
+    override def simplify: Drawable = this match {
+      case Relocate(x, y, Relocate(_x, _y, drawable)) => Relocate(x + _x, y + _y, drawable.simplify)
+      case Relocate(0, 0, drawable) => drawable.simplify
+      case Relocate(_, _, drawable) => this.change(drawable.simplify)
+    }
   }
 }
--- scala/src/main/scala/figure_parsers/mainFigureParsers.scala
+++ scala/src/main/scala/figure_parsers/mainFigureParsers.scala
@@ -1,9 +1,8 @@
 package figure_parsers
 
+import figure_parsers.cases.Coordinates2D
 import figure_parsers.cases.objects._
-import tadp.internal.TADPDrawingAdapter
-
-import scala.io.Source
+import figure_parsers.internal.Figure.{Circle, Group}
 
 object mainFigureParsers extends App {
   val triangle = "triangulo[0 @ 100, 200 @ 300, 400 @ 500]"
@@ -44,19 +43,25 @@ object mainFigureParsers extends App {
 
   val parsedFiguresToSimplify = drawableParser(everySimplificationUnlessGroupColor)
 
-  val resultOfSimplify = simplify(parsedFiguresToSimplify.get.parsed)
+  val resultOfSimplify = parsedFiguresToSimplify.get.parsed.simplify
 
   val groupWithCommonColour = "grupo(escala[1.45, 1.45](rectangulo[0 @ 0, 400 @ 400]),escala[1.45, 1.45](rectangulo[0 @ 0, 400 @ 400]),escala[1.45, 1.45](rectangulo[0 @ 0, 400 @ 400]))"
 
   val groupWithCommonColourParsed = drawableParser(groupWithCommonColour)
 
-  val resultOfSimplify2 = simplify(groupWithCommonColourParsed.get.parsed)
+  val resultOfSimplify2 = groupWithCommonColourParsed.get.parsed.simplify
 
   val groupWithNullTransformations = "grupo(escala[1, 1](circulo[0 @ 5, 10]),traslacion[0, 0](circulo[0 @ 5, 10]),rotacion[0](rectangulo[100 @ 200, 300 @ 400])))"
 
   val groupWithNullTransformationsParsed = drawableParser(groupWithNullTransformations)
 
-  val resultOfSimplify3 = simplify(groupWithNullTransformationsParsed.get.parsed)
+  val resultOfSimplify3 = groupWithNullTransformationsParsed.get.parsed.simplify
+
+  val coo = Group(Circle(Coordinates2D(2, 5), 4)::Nil)
+
+  val d2 = Group(Circle(Coordinates2D(2, 5), 3)::Nil)
+
+  val as = coo == d2
 
   val dummyImplicit = 2
 }
